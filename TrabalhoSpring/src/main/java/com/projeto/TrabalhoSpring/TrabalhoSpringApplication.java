d15e3798a3332effec9a8ac752bb88887a4f3821
PedroHenrique <pdhenrialves16@gmail.com>
2025-09-25 11:33:03 -0300

    ProjetoSpring

---
+++ TrabalhoSpring/src/main/java/com/projeto/TrabalhoSpring/TrabalhoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.TrabalhoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoSpringApplication.class, args);
+	}
+
+}
